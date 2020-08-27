fb27386ebb94bbbaf7808d0e8e9b9f716286f016
大钊 <samciu@dazhaodeMacBook-Pro.local>
2020-08-27 19:47:51 +0800

    bugfix

--- src/components/CheckoutForm/CheckoutForm.js
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -83,7 +83,7 @@ function CheckoutForm() {
 
     emptyCart();
 
-    navigate('success', { state: { orderId } });
+    navigate('/success', { state: { orderId } });
   };
 
   const calculateOrderCosts = async values => {
